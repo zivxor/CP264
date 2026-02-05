2075dd0b4104164799d693b5e6a0ddc8b9f86d8b
Zivkovic4716 <luka_z@icloud.com>
2026-02-04 19:19:27 -0500

    updated mysort.c and mysort.c and added ptest for it

---
+++ a4/mysort.c
@@ -0,0 +1,74 @@
+/**
+ * Use selection sort algorithm to sort array of pointers such that their
+ * pointed values are in increasing order.
+ *
+ * @param *a[] - array of void pointers.
+ * @param left - the start index of pointer in array.
+ * @param right - the end index of pointer in array
+ */
+void selection_sort(void *a[], int left, int right) {
+  for (int i = left; i <= right; i++) { // loop from left until right index
+    int min_idx = i;
+    for (int j = i + 1; j <= right; j++) {
+      if (*(float *)a[j] < *(float *)a[min_idx]) {
+        min_idx = j;
+      }
+    }
+    // Swap a[i] and a[min_idx]
+    void *temp = a[i];
+    a[i] = a[min_idx];
+    a[min_idx] = temp;
+  }
+}
+
+/**
+ * Use quick sort algorithm to sort array of pointers such that their pointed
+ * values are in increasing order.
+ *
+ * @param *a[] - array of void pointers.
+ * @param left - the start index of pointer in array.
+ * @param right - the end index of pointer in array
+ */
+void quick_sort(void *a[], int left, int right) {
+  if (left < right) {
+    int i = left + 1;
+    int j = right;
+    while (i <= j) {
+      while (i <= right && cmp(a[i], a[left]) <= 0)
+        i++;
+      while (j >= left && cmp(a[j], a[left]) > 0)
+        j--;
+      if (i < j) {
+        swap(&a[i], &a[j]);
+      }
+    }
+    swap(&a[left], &a[j]);
+    quick_sort(a, left, j - 1);
+    quick_sort(a, j + 1, right);
+  }
+}
+
+/**
+ * Use either selection or quick sort algorithm to sort array of pointers
+ * such that their pointed values are in order defined by the comparison
+ * function
+ *
+ * @param *a[] - array of void pointers.
+ * @param left - the start index of pointer in array.
+ * @param right - the end index of pointer in array
+ * @param (*cmp) - pointer to a comparison function for comparing pointers
+ *                by their pointed values.
+ */
+void cmp_sort(void *a[], int left, int right, int (*cmp)(void *, void *)) {
+  for (int i = left; i < right; i++) {
+    int min = i;
+    for (int j = i + 1; j <= right; j++) {
+      if (cmp(a[j], a[min]) < 0) {
+        min = j;
+      }
+    }
+    if (min != i) {
+      swap(a[i], a[min]);
+    }
+  }
+}
--- a4/mysort.h
+++ a4/mysort.h
@@ -1,8 +1,8 @@
-#ifndef MYSORT.H
-#define MYSORT.H
+#ifndef MYSORT_H
+#define MYSORT_H
 /**
- * Use selection sort algorithm to sort array of pointers such that their pointed values 
- * are in increasing order.
+ * Use selection sort algorithm to sort array of pointers such that their
+ * pointed values are in increasing order.
  *
  * @param *a[] - array of void pointers.
  * @param left - the start index of pointer in array.
@@ -11,24 +11,25 @@
 void selection_sort(void *a[], int left, int right);
 
 /**
- * Use quick sort algorithm to sort array of pointers such that their pointed values 
- * are in increasing order.
+ * Use quick sort algorithm to sort array of pointers such that their pointed
+ * values are in increasing order.
  *
- * @param *a[] - array of void pointers. 
+ * @param *a[] - array of void pointers.
  * @param left - the start index of pointer in array.
  * @param right - the end index of pointer in array
  */
-void quick_sort(void *a[], int left, int right);
+void quick_sort(void *a[], int left, int right) {}
 
 /**
- * Use either selection or quick sort algorithm to sort array of pointers 
- * such that their pointed values are in order defined by the comparison function
+ * Use either selection or quick sort algorithm to sort array of pointers
+ * such that their pointed values are in order defined by the comparison
+ * function
  *
- * @param *a[] - array of void pointers. 
+ * @param *a[] - array of void pointers.
  * @param left - the start index of pointer in array.
  * @param right - the end index of pointer in array
- * @param (*cmp) - pointer to a comparison function for comparing pointers 
+ * @param (*cmp) - pointer to a comparison function for comparing pointers
  *                by their pointed values.
  */
-void cmp_sort(void *a[], int left, int right, int (*cmp)(void*, void*) );
-#endif MYSORT.H
+void cmp_sort(void *a[], int left, int right, int (*cmp)(void *, void *));
+#endif
