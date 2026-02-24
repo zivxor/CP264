eaaed14f34eebf7c83be05eb7f8278324a8784e8
Zivxor <zivxor@gmail.com>
2026-02-24 17:34:09 -0500

    Complete A6

--- a6/common.h
+++ a6/common.h
@@ -4,25 +4,33 @@
  Author:  HBF
  Version: 2026-02-12
  --------------------------------------------------
- */ 
+ */
 #ifndef COMMON_H
 #define COMMON_H
 
-/* 
+/*
  Node structure of linked list queue and stack for postfix expression operations
  int data - represent operand, operator, or parenthesis according to the type
  int type - 0:operand; 1:operator; 2:left parenthesis 3:right parenthesis
  */
 typedef struct node {
-	int data;
-	int type;
-	struct node *next;
+  int data;
+  int type;
+  struct node *next;
 } NODE;
 
 NODE *new_node(int data, int type);
 void clean(NODE **npp);
 void display(NODE *np);
 int mytype(char c);
+int priority(char op);
+
+void clean(NODE **npp);
+
+void display(NODE *np);
+
+int mytype(char c);
+
 int priority(char op);
 
 #endif
--- a6/expression.c
+++ a6/expression.c
@@ -13,10 +13,62 @@
 QUEUE infix_to_postfix(char *infixstr) {
   QUEUE que = {0, NULL, NULL};
   STACK st = {0, NULL};
-  float total = 0.0f;
-  char *token = strtok(infixstr, " ");
-  while (token != NULL) {
+  int num = 0, sign = 1;
+  int in_number = 0; // track if we are building a number
+  char *p = infixstr;
+
+  while (*p) {
+    if (*p == '-' && (p == infixstr || *(p - 1) == '(')) {
+      sign = -1;
+    } else if (mytype(*p) == 0) {
+      // digit
+      in_number = 1;
+      num = num * 10 + (*p - '0');
+    } else {
+      // non-digit: if we were building a number, enqueue it now
+      if (in_number) {
+        enqueue(&que, new_node(num * sign, 0)); // type 0 = operand
+        num = 0;
+        sign = 1;
+        in_number = 0;
+      }
+
+      if (mytype(*p) == 1) {
+        // operator: pop higher/equal precedence operators from stack
+        while (st.top && mytype(st.top->data) == 1 &&
+               priority(st.top->data) >= priority(*p)) {
+          enqueue(&que, pop(&st));
+        }
+        push(&st, new_node(*p, 1)); // type 1 = operator
+
+      } else if (mytype(*p) == 2) {
+        // '('
+        push(&st, new_node(*p, 2)); // type 2 = left paren
+
+      } else if (mytype(*p) == 3) {
+        // ')': pop until '('
+        while (st.top && st.top->data != '(') {
+          enqueue(&que, pop(&st));
+        }
+        if (st.top && st.top->data == '(') {
+          free(pop(&st)); // discard '('
+        }
+      }
+    }
+    p++;
   }
+
+  // If the expression ended while building a number
+  if (in_number) {
+    enqueue(&que, new_node(num * sign, 0));
+  }
+
+  // Pop remaining operators
+  while (st.top) {
+    enqueue(&que, pop(&st));
+  }
+
+  return que;
 }
 
 /*
@@ -24,12 +76,68 @@ QUEUE infix_to_postfix(char *infixstr) {
  * @parame queue - postfix expression in queue of QUEUE type.
  ^ @return - value of postfix expression
 */
-int evaluate_postfix(QUEUE queue);
+int evaluate_postfix(QUEUE queue) {
+  NODE *curr = queue.front;
+  STACK st = {0, NULL};
+  int type = 0;
+
+  while (curr) {
+    type = curr->type;
+    if (type == 0) {
+      // operand
+      push(&st, new_node(curr->data, 0));
+    } else if (type == 1) {
+      // operator
+      NODE *nb = pop(&st);
+      NODE *na = pop(&st);
+      if (!na || !nb) {
+        // malformed postfix; avoid segfault
+        return 0;
+      }
+      int b = nb->data;
+      int a = na->data;
+      free(na);
+      free(nb);
+
+      int result = 0;
+      switch (curr->data) {
+      case '+':
+        result = a + b;
+        break;
+      case '-':
+        result = a - b;
+        break;
+      case '*':
+        result = a * b;
+        break;
+      case '/':
+        result = a / b;
+        break;
+      }
+
+      // result is an operand
+      push(&st, new_node(result, 0));
+    }
+    curr = curr->next;
+  }
+
+  NODE *res = pop(&st);
+  if (!res)
+    return 0;
+  int final_result = res->data;
+  free(res);
+  return final_result;
+}
 
 /*
- * Evaluate and return the value of infix expression passed by string infixstr,
- * using infix_to_postfix() and evaluate_postfix() functions.
+ * Evaluate and return the value of infix expression passed by string
+ * infixstr, using infix_to_postfix() and evaluate_postfix() functions.
  * @param infixstr - string of infix expression.
  * @return - value of the infix expression.
  */
-int evaluate_infix(char *infixstr);
+int evaluate_infix(char *infixstr) {
+  QUEUE que = infix_to_postfix(infixstr);
+  int result = evaluate_postfix(que);
+  clean_queue(&que);
+  return result;
+}
